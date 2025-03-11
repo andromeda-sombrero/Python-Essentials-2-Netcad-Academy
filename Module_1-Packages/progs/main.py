eadd841ec171b7de8da074bfd979c7a6f45f82ba
Semukelo Mathe <semmathe@gmail.com>
2025-03-12 00:27:34 +0200

    module 1 packages

---
+++ Module_1-Packages/progs/main.py
@@ -0,0 +1,13 @@
+from sys import path
+
+path.append("..\\modules")
+
+from module import sum1, prod1
+
+zeroes = [i + 1 for i in range(5)]
+ones = [i + 1 for i in range(5)]
+print(sum1(zeroes))
+print(prod1(ones))
+
+# for p in path:
+    # print(p)

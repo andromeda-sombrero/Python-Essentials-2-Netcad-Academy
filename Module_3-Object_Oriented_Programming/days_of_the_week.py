19cc39b35998e4c0a6a41e36b5f3593a3f3309df
andromeda sombrero <sgvonjoli@gmail.com>
2025-03-18 10:31:56 +0200

    lab exercise 5 - days of the week

--- Module_3-Object_Oriented_Programming/days_of_the_week.py
+++ Module_3-Object_Oriented_Programming/days_of_the_week.py
@@ -11,58 +11,56 @@ the following set:
 Mon Tue Wed Thu Fri Sat Sun
 
 Invoking the constructor with an argument from outside this set should raise
-the WeekDayError exception (define it yourself; don't worry, we'll talk about
-the objective nature of exceptions soon). The class should provide the
-following facilities:
+the WeekDayError exception (define it yourself).
+The class should provide the following facilities:
 
-objects of the class should be "printable", i.e. they should be able to
+1. Objects of the class should be "printable", i.e. they should be able to
 implicitly convert themselves into strings of the same form as the constructor
 arguments;
-the class should be equipped with one-parameter methods called add_days(n) and
+2. The class should be equipped with one-parameter methods called add_days(n) and
 subtract_days(n), with n being an integer number and updating the day of week
 stored inside the object in the way reflecting the change of date by the
 indicated number of days, forward or backward.
 all object's properties should be private;
 """
 
+
 class WeekDayError(Exception):
     pass
-	
+
 
 class Weeker:
-    #
-    # Write code here.
-    #
+    __weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    __chosen_day = ""
 
     def __init__(self, day):
-        #
-        # Write code here.
-        #
+        self.__day = day
+
+    def get_day(self):
+        if self.__day != ("Mon" or "Tue" or "Wed" or "Thu" or "Fri" or "Sat" or "Sun"):
+            raise WeekDayError
+        else:
+            return self.__day
 
     def __str__(self):
-        #
-        # Write code here.
-        #
+        return Weeker.__chosen_day
 
     def add_days(self, n):
-        #
-        # Write code here.
-        #
+        Weeker.__chosen_day = Weeker.__weekdays[7 % len(Weeker.__weekdays)]
+        return Weeker.__chosen_day
 
     def subtract_days(self, n):
-        #
-        # Write code here.
-        #
+        Weeker.__chosen_day = Weeker.__weekdays[-(n % len(Weeker.__weekdays))]
+        return Weeker.__chosen_day
 
 
 try:
-    weekday = Weeker('Mon')
+    weekday = Weeker("Mon")
     print(weekday)
     weekday.add_days(15)
     print(weekday)
     weekday.subtract_days(23)
     print(weekday)
-    weekday = Weeker('Monday')
+    weekday = Weeker("Monday")
 except WeekDayError:
     print("Sorry, I can't serve your request.")
-    
